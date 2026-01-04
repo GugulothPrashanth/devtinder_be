9d1af14b1059bb3eed9cec8b8d34b8037626a59d
Prashanth <prashanthg9849@gmail.com>
2026-01-04 19:30:52 +0530

    fix env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://prashanth:Vsgk1Q9DxJ6zJZaq@devtinder.ew4hsfs.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
 module.exports = connectDB;
